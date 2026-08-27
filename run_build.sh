ea7b359bee958c31025f522503a3332eda2e6361
mx57 <38256814+mx57@users.noreply.github.com>
2026-08-27 18:26:46 +0000

    🎨 UI/UX: Replace deprecated icons with AutoMirrored versions

--- run_build.sh
+++
@@ -1,4 +0,0 @@
-cd /home/jules/proj/code/go_libs/yggdrasil-go
-PATH=/home/jules/go/bin:$PATH /home/jules/go/bin/gomobile bind -target android -androidapi 26 -tags mobile -o /app/app/libs/holowbark.aar -ldflags="-s -w -checklinkname=0 -extldflags=-Wl,-z,max-page-size=16384" ./contrib/mobile ./src/config ./contrib/awgmobile
-cd /app
-./gradlew assembleDebug
